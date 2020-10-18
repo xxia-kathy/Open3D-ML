4833e1aeccdf8b4d679ee583a4dfa570ffbd192e
xiaxia <kathyxxj@service-inf-drzs-vpn-078.ethz.ch>
2020-10-18 12:41:32 +0200

    add new dataset

--- ml3d/datasets/__init__.py
+++ ml3d/datasets/__init__.py
@@ -8,8 +8,9 @@ from .parislille3d import ParisLille3D
 from .toronto3d import Toronto3D
 from .customdataset import Custom3D
 from .semantic3d import Semantic3D
+from .ahat import AHAT
 from .utils import *
 __all__ = [
     'SemanticKITTI', 'S3DIS', 'Toronto3D', 'ParisLille3D', 'Semantic3D',
-    'Custom3D', 'utils'
+    'Custom3D', 'AHAT', 'utils'
 ]
